d0cc41b95021e9c781a04c48cef51d7984708e5f
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-23 01:39:04 +0200

    Adding react router and app pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Switch, Route, HashRouter as Router } from 'react-router-dom';
 import './App.css';
+import Main from './pages/Main';
+import CountryList from './pages/CountryList';
+import Country from './pages/Country';
 
 function App() {
   return (
+  <Router basename="/"> 
     <div className="App">
-     <h1>Hello</h1>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/continent" component={CountryList} />
+          <Route exact path="/country" component={Country} />
+        </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/pages/CountryList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountryList = () => {
+  return (
+    <div>
+      <h1>Country List page</h1>
+    </div>
+  )
+}
+
+export default CountryList;
