3103a1f444bf8dba3804daa9c2e14daec8190538
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-23 10:25:05 +0200

    Add data from store state to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Main = () => {
+  const { continents } = useSelector((state) => state.country)
+
   return (
     <div>
       <h1>Main page</h1>
+      <ul>
+        {continents.map((cont, idx) => <li key={idx}>{cont}</li>)}
+      </ul>
     </div>
   )
 }
