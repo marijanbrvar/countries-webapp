b7466605cac95be8d9f15772e37d21167dfc14d3
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-23 23:55:28 +0200

    Add react middleware to store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import countryReducer from './country';
+import api from "./middleware/api";
 
 export default configureStore({
   reducer: {
     country: countryReducer,
   },
+  middleware: [
+    ...getDefaultMiddleware(),
+    api
+  ]
 });
