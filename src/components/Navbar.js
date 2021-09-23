b9a9c774b5ed582e4857be059a1bcfe61011462d
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-23 11:19:18 +0200

    Add icons to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
-  const continent = useSelector((state) => state.country.currentContinent);
+  const {currentContinent } = useSelector((state) => state.country);
   return (
     <div>
-      <h1>{continent}</h1>
+      <h1>{currentContinent}</h1>
+      <i className="material-icons mx-1">mic</i>
+      <i className="material-icons">settings</i>
     </div>
   )
 }
